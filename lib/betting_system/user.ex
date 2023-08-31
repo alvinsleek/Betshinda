b852f77d474939211f89326fad8c73236bf14a81
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-08-31 17:12:22 -0400

    user schema

---
+++ lib/betting_system/user.ex
@@ -0,0 +1,20 @@
+defmodule BettingSystem.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :msisdn, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:first_name, :last_name, :email, :msisdn])
+    |> validate_required([:first_name, :last_name, :email, :msisdn])
+  end
+end
